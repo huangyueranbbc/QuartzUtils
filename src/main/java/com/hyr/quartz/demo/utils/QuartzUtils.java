f3b20457d0f16e80815c24620c146a418089a3a0
huangyueran <492736173@qq.com>
2018-11-07 10:22:37 +0800

    注释

--- src/main/java/com/hyr/quartz/demo/utils/QuartzUtils.java
+++ src/main/java/com/hyr/quartz/demo/utils/QuartzUtils.java
@@ -10,11 +10,8 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /*******************************************************************************
- * 版权信息：博睿宏远科技发展有限公司
- * Copyright: Copyright (c) 2007博睿宏远科技发展有限公司,Inc.All Rights Reserved.
- *
  * @date 2018-11-11 下午 11:11
- * @author: <a href=mailto:huangyr@bonree.com>黄跃然</a>
+ * @author: <a href=mailto:huangyr>黄跃然</a>
  * @Description: Quartz工具类
  ******************************************************************************/
 public class QuartzUtils {
